e5cd52771b557c3640a28bbf9eeca06f666d5718
mike <mike.ouimet@gmail.com>
2017-12-17 00:33:05 -0800

    sped up

--- 2017/day15/day15.py
+++ 2017/day15/day15.py
@@ -1,1 +1,46 @@
 import day15_cy
+'''
+import time
+start = time.time()
+A = 722
+B = 354
+
+
+
+mult_A = 16807
+mult_B = 48271
+
+div = 2147483647
+
+#A =65
+#B = 8921
+
+big =  2**16
+
+
+count = 0
+tmp =0
+size = 50000
+for i in range(size):
+
+	A = (A * mult_A) % div
+	B = (B * mult_B) % div
+	while A %4 != 0:
+		A = (A * mult_A) % div
+	while B%8 != 0:
+		B = (B * mult_B) % div
+
+
+
+	if A%big == B%big:
+		#print(i)
+		count +=1
+		
+
+	#bA = bin(A)[2:].rjust(16,'0')
+	#bB = bin(B)[2:].rjust(16,'0')
+
+
+print(time.time() -start)
+
+'''
