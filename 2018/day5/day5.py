fc1e23503b5870f12939acfc2bb3a37957d56e67
mike <mike.ouimet@gmail.com>
2018-12-05 12:31:10 -0800

    faster 5

--- 2018/day5/day5.py
+++ 2018/day5/day5.py
@@ -15,22 +15,23 @@ def fully_react(s):
 				popped = True
 		if not popped:
 			stack.append(letter)
-	return len(stack)
+	return stack
 
 
 
 
 new_s = fully_react(line)
 
-print('part 1: ', new_s)
+print('part 1: ', len(new_s))
 
+line = ''.join(new_s)
 s = time.time()
 lenred = 50000
 for letter in 'abcdefghijklmnopqrstuvwxyz':
 	new_s = line.replace(letter, '')
 	new_s = new_s.replace(letter.upper(), '')
 	reduced_s = fully_react(new_s)
-	lenred  = min(lenred, reduced_s)
+	lenred  = min(lenred, len(reduced_s))
 
 print('part 2: ', lenred)
 print('Time', time.time() - s)
